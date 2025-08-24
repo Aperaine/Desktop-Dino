e312e25e0177eea256bf60a454894e159c3215ab
Aperaine <Aperaine@users.noreply.github.com>
2025-08-24 22:51:20 +0900

    Made animation tree

--- dinoDesktop/scenes/dino.gd
+++ dinoDesktop/scenes/dino.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600
 const GRAVITY_MULTIPLIER = 3
+
+@export var crouch := false
+
 
 func _ready() -> void:
 	set_physics_process(false)
@@ -19,8 +25,12 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		jump()
+	
+	crouch = Input.is_action_pressed("crouch")
 
 	move_and_slide()
 
 func jump() -> void:
+	animation_tree.set("parameters/conditions/run", true)
+	
 	velocity.y = JUMP_VELOCITY
