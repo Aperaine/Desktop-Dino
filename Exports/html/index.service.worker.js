c0590d87a7ff95f4431aab0afda745188dcc310f
Aperaine <Aperaine@users.noreply.github.com>
2025-09-06 20:00:05 +0900

    Undid the score mistake

--- Exports/html/index.service.worker.js
+++ Exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757144379|512289340';
+const CACHE_VERSION = '1757156319|55098222';
 /** @type {string} */
 const CACHE_PREFIX = 'Desktop Dino-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
